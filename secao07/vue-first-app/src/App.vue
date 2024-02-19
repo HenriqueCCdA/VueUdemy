3f031e422930084bc5aae2672ddd0cf7c2cd3b81
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-02-19 17:35:38 -0300

    Aula 86

---
+++ secao07/vue-first-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '01234 45678 90',
+          email: 'manuel@localhost.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jone',
+          phone: '0987 65421 21',
+          email: 'juliel@localhost.com',
+        },
+      ]
+    }
+  },
+}
+
+</script>
