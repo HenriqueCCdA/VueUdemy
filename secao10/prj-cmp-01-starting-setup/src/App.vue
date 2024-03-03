b86bc10545cfd188cda8f0931f47037759fc3eeb
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-03 17:29:51 -0300

    aula 128

---
+++ secao10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="s in storedResources" :key="s.id">
+      {{ s }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        },
+      ]
+    }
+  }
+}
+
+</script>
