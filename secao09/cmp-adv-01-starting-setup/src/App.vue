23cb8687a20488e3be11fc033c032fda9f5de399
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-03 17:09:53 -0300

    aula 124

--- secao09/cmp-adv-01-starting-setup/src/App.vue
+++ secao09/cmp-adv-01-starting-setup/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
+    <button @click="setSelectedComponents('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponents('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+import TheHeader from './components/layout/TheHeader.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -30,6 +30,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponents(cmp) {
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
@@ -42,3 +47,4 @@ body {
   margin: 0;
 }
 </style>
+./components/layout/TheHeader.vue
