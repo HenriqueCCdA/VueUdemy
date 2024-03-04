4d88b0a830e9b57a05c6a2127349910731d573cf
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-04 13:08:44 -0300

    Aula 132

--- secao10/prj-cmp-01-starting-setup/src/App.vue
+++ secao10/prj-cmp-01-starting-setup/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <ul>
-    <li v-for="s in storedResources" :key="s.id">
-      {{ s }}
-    </li>
-  </ul>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
 export default {
+  components: {
+    StoredResources,
+    TheHeader,
+  },
   data() {
     return {
       storedResources: [
@@ -29,3 +33,19 @@ export default {
 }
 
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
