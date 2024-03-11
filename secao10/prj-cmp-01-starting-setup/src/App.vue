88cb4f31208b6651278373ed5a47157690ddc003
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-10 21:01:57 -0300

    aula 136

--- secao10/prj-cmp-01-starting-setup/src/App.vue
+++ secao10/prj-cmp-01-starting-setup/src/App.vue
@@ -1,35 +1,17 @@
 <template>
   <the-header title="RememberMe"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-resource></the-resource>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResource from './components/learning-resources/TheResource.vue';
 
 export default {
   components: {
-    StoredResources,
     TheHeader,
+    TheResource,
   },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org'
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.com'
-        },
-      ]
-    }
-  }
 }
 
 </script>
