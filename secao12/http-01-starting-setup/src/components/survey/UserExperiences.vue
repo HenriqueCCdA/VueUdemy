768fc59bea920a631232d06eeb702a999396cd60
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-03-24 17:10:16 -0300

    m

---
+++ secao12/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExpeciences">Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExpeciences() {
+      fetch('https://vue-http-demo-8a376-default-rtdb.firebaseio.com/surveys.json').then(
+        response => {
+          if (response.ok) {
+            return response.json();
+          }
+        }
+      ).then(data => {
+        const results = []
+
+        for (const id in data) {
+          results.push({id, name: data[id].name, rating: data[id].rating});
+        }
+        this.results = results;
+      });
+    },
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
